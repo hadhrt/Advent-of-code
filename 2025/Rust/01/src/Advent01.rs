6a2c3958b5c9df17503b3a4f2497eae6f076b5dc
CH <hadhrt@gmail.com>
2025-12-05 11:04:38 +0100

    added day1 rust solution

---
+++ 2025/Rust/01/src/Advent01.rs
@@ -0,0 +1,70 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn part1(lines: &Vec<&str>) -> i32 {
+    let mut number_of_zero_pos = 0;
+    let mut dial_position = 50;
+    for line in lines {
+        match &line[..1] {
+            "R" => {
+                dial_position = (dial_position + line[1..].parse::<i32>().expect("invalid number "))
+                    .rem_euclid(100)
+            }
+            "L" => {
+                dial_position = (dial_position - line[1..].parse::<i32>().expect("invalid number "))
+                    .rem_euclid(100)
+            }
+            _ => println!("invalid line: {}", line),
+        }
+        // println!("dial Position: {}", dial_position);
+        if dial_position == 0 {
+            number_of_zero_pos += 1
+        }
+    }
+    number_of_zero_pos
+}
+
+fn part2(lines: &Vec<&str>) -> i32 {
+    let mut final_value = 0;
+    for line in lines {
+        match &line[..] {
+            "1" => {
+                final_value += 1
+            }
+            _ => println!("invalid line: {}", line),
+        }
+    }
+    final_value
+}
+
+fn main() {
+    let mut file = File::open("./../../Inputs/example.input").expect("Faild to read InputFile");
+    // let mut file = File::open("./../../Inputs/01.input").expect("Faild to read InputFile");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to convert Input File");
+    let lines = contents.lines().collect::<Vec<&str>>();
+
+    // Part 1
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let result_p1 = part1(&lines);
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let duration_p1 = end - start;
+    println!("Result Part1: {}", result_p1);
+    println!(
+        "Time   Part1: {} sec",
+        (duration_p1.as_millis() as f32) / 1000.0
+    );
+
+    // Part 2
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let result_p1 = part2(&lines);
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let duration_p1 = end - start;
+    println!("Result Part2: {}", result_p1);
+    println!(
+        "Time   Part2: {} sec",
+        (duration_p1.as_millis() as f32) / 1000.0
+    );
+}
---
+++ 2025/Rust/advent_template.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn part1(lines: Vec<&str>) -> i32 {
+    let mut final_value = 0;
+    for line in lines {
+        match &line[..] {
+            "1" => {
+                final_value += 1
+            }
+            _ => println!("invalid line: {}", line),
+        }
+    }
+    final_value
+}
+
+fn part2(lines: &Vec<&str>) -> i32 {
+    let mut final_value = 0;
+    for line in lines {
+        match &line[..] {
+            "1" => {
+                final_value += 1
+            }
+            _ => println!("invalid line: {}", line),
+        }
+    }
+    final_value
+}
+
+fn main() {
+    let mut file = File::open("./../../Inputs/example.input").expect("Faild to read InputFile");
+    // let mut file = File::open("./../../Inputs/01.input").expect("Faild to read InputFile");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to convert Input File");
+    let lines = contents.lines().collect::<Vec<&str>>();
+
+    // Part 1
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let result_p1 = part1(&lines);
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let duration_p1 = end - start;
+    println!("Result Part1: {}", result_p1);
+    println!(
+        "Time   Part1: {} sec",
+        (duration_p1.as_millis() as f32) / 1000.0
+    );
+
+    // Part 2
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let result_p1 = part2(&lines);
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let duration_p1 = end - start;
+    println!("Result Part2: {}", result_p1);
+    println!(
+        "Time   Part2: {} sec",
+        (duration_p1.as_millis() as f32) / 1000.0
+    );
+}
