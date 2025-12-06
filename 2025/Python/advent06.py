45d62c91ce91cb6d8bcb7a15874c9a70ad0dc897
hadhrt <haddi.hrtg@gmail.com>
2025-12-06 14:43:57 +0100

    optimized day6 p2

--- 2025/Python/advent06.py
+++ 2025/Python/advent06.py
@@ -1,24 +1,6 @@
 import timeit
 import math
 
-ADJ_OFFSETS = (-1 - 1j, -1 + 0j, -1 + 1j, 0 - 1j, 0 + 1j, 1 - 1j, 1 + 0j, 1 + 1j)
-
-ADJ_OFFSETS_NO_DIAG = (
-    -1 + 0j,
-    0 - 1j,
-    0 + 1j,
-    1 + 0j,
-)
-
-
-def print_grid(grid):
-    print_string = ""
-    for coord, val in grid.items():
-        if coord.imag == 0:
-            print_string += "\n"
-        print_string += val
-    print(print_string)
-
 
 def p1(lines):
     value = 0
@@ -39,32 +21,20 @@ def p1(lines):
 
 
 def p2(lines):
-    value = 0
-    numbers = [
-        int("".join(numberstring).strip())
-        for numberstring in list(zip(*map(list, lines[:-1])))
-        if "".join(numberstring).strip() != ""
-    ]
-    # operator line defines the amount of numbers per problem
-    # split string at operands but keep operands by inserting magic value as separator
-    operators = [
-        (operator_string[0], len(operator_string) - 1)
-        for operator_string in (
-            lines[-1].replace("+", "¬¬+").replace("*", "¬¬*") + " "
-        ).split("¬¬")[1:]
-    ]
+    answer = 0
+    operands = []
+    columns = ["".join(c) for c in zip(*lines)]
 
-    numberiter = iter(numbers)
-    for operator in operators:
-        problem_numbers = [next(numberiter) for _ in range(operator[1])]
-        if operator[0] == "+":
-            value += sum(problem_numbers)
-        elif operator[0] == "*":
-            value += math.prod(problem_numbers)
-        else:
-            return
-
-    return value
+    for column in columns[::-1]:
+        if column.strip() != "":
+            operands.append(int(column[:-1]))
+            if column[-1] == "+":
+                answer += sum(operands)
+                operands = []
+            elif column[-1] == "*": 
+                answer += math.prod(operands)
+                operands = []
+    return answer
 
 
 # f = open(r"2025/Inputs/example.input", "r")
